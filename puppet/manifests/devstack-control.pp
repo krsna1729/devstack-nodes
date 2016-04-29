102124e5ed0d379ebab81ae62b3af23301c72f83
krsna1729 <krsna1729@users.noreply.github.com>
2016-04-29 14:10:03 -0700

    Update devstack-control.pp

--- puppet/manifests/devstack-control.pp
+++ puppet/manifests/devstack-control.pp
@@ -27,6 +27,13 @@ vcsrepo {'/home/vagrant/networking-onos':
 
 $hosts = hiera('hosts')
 
+file { '/etc/hosts':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    content => template('/vagrant/puppet/templates/hosts.erb')
+}
+
 file { '/home/vagrant/devstack/local.conf':
     ensure  => present,
     owner   => 'vagrant',
