7636088fc3846ae0da2910478fcb8234c4d1e4d0
krsna1729 <krsna1729@users.noreply.github.com>
2016-07-28 22:00:42 -0700

    Helper scripts for testing

---
+++ start1.sh
@@ -0,0 +1,10 @@
+sudo rm -rf /tmp/bess
+sudo mkdir -p /tmp/bess
+#screen -S bess -d -m "sudo /opt/bess/bin/bessd -fk > bess.log"
+sudo /opt/bess/bin/bessd -k
+
+DPID=$(hostname | cut -d- -f3)
+/opt/bess/bin/bessctl daemon reset
+#/vagrant/bess_of_agent.py -d $DPID -c '192.168.60.20' 
+/vagrant/bess_of_agent.py -f /vagrant/OVS-OF-compute-1.pcap -i 192.168.50.21
+
